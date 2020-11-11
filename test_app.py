76d068e8c9dc953cf08bffa029a1c728b5206b82
Mickcheng <58817514+Mickcheng@users.noreply.github.com>
2020-11-11 21:58:32 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,49 +4,20 @@ import requests
 
 
 class FlaskTests(unittest.TestCase):
-	
-	
 		
-		
-	def test_a_index(self):
+	def test_index(self):
 		responce = requests.get('http://localhost:5000')
-		self.assertEqual(responce.status_code, 200)
+		self.assertEqual(responce.status_code, 200)		
 		
-
-		
-		
-	def test_b_get_sentiment(self):
-	
-	
+	def test_get_sentiment(self):
 		self.params = " un texte "
-		
-		
-		responce = requests.get('http://localhost:5000',data=self.params)
-		self.assertEqual(responce.status_code, 200)
-		
-		
+		self.assertEqual(get_sentiment("un texte"), "neutre")	
 	
-	
-	def test_c_sent(self):
-		
-		
-		
+	def test_sent(self):
 		params = {'sentences': 'Positive'}
-		
 		responce = requests.get('http://localhost:5000',data=params)
 		self.assertEqual(responce.status_code, 200)
 		
-
-		
-		
-	
-	
-	
-	
-	
-
-		
-	
 	
 if __name__ == '__main__':
 	unittest.main()		
